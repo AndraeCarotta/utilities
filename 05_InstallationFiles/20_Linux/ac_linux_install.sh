55b5f61920340ff0a58b70aa24f674c4d0af9a27
AndraeCarotta <andrae.carotta@hotmail.com>
2025-03-12 23:34:06 +0100

    AC: update linux config

--- 05_InstallationFiles/20_Linux/ac_linux_install.sh
+++ 05_InstallationFiles/20_Linux/ac_linux_install.sh
@@ -1,5 +1,22 @@
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt update && sudo apt upgrade -y
+
+# set some settings
+sudo timedatectl set-timezone Europe/Berlin
+
+# install some basic developer stuff
+sudo apt install -y curl
+sudo apt install -y wget
+sudo apt install -y vim
+sudo apt install -y neovim
+sudo apt install -y tmux
+sudo apt install -y htop
+sudo apt install -y tree
+sudo apt install -y unzip
+sudo apt install -y zip
+sudo apt install -y git
+sudo apt install -y build-essential
+#sudo apt install -y cmake
+sudo apt install -y code
 
 # git settings
 git config --global user.name AndraeCarotta
@@ -8,6 +25,7 @@ git config --global user.email "andrae.carotta@hotmail.com"
 # python
 sudo apt upgrade python3
 sudo apt install python3-pip
+sudo apt install -y python3-venv
 
 # docker and portainer
 curl -fsSL https://get.docker.com -o get-docker.sh
